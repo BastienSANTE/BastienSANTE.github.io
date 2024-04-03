faa53a3c7619bb680257fe45b22b878fd53e0aca
BastienSANTE <118430107+BastienSANTE@users.noreply.github.com>
2024-04-03 18:53:22 +0200

    Début de la nouvelle version du site

---
+++ main.js
@@ -0,0 +1,15 @@
+let image_count = 8;
+let image_ident = [];
+  
+workgrid = document.getElementById("work_grid");
+persgrid = document.getElementById("pers_grid");
+
+  for(let i = 0; i  < image_count; i++){
+    image_ident.push(Math.floor(Math.random()*image_count));
+    console.log(i);
+  }
+
+  for (i in image_ident){
+    workgrid.innerHTML += `<div><img src="./Thumbnails/Work/${i}.png"></img></div>`;
+    persgrid.innerHTML += `<div><img src="./Thumbnails/Pers/${i}.png"></img></div>`;
+  }
